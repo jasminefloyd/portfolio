52d6ae2bb9a366bbcf965066299c9d12a62731a8
jasminefloyd <42697356+jasminefloyd@users.noreply.github.com>
2024-10-14 23:08:06 -0400

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,23 +5,23 @@ export default function Footer() {
 
     return (
         <div className="site-footer">
-            <img className="footer-logo" src="./src/assets/jf_logo.png" />
+            <img className="footer-logo" src="./public/jf_logo.png" />
             <p>&copy; All Rights Reserved {currentYear}</p>
             <div className="icon-tray">
                 <ul className="icon-list">              
                     <li className="linkedin">
                         <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-                            <img src="./src/assets/linkedin.png" alt="LinkedIn" />
+                            <img src="./public/linkedin.png" alt="LinkedIn" />
                         </a>
                     </li>
                     <li className="github">
                         <a href="https://www.github.com" target="_blank" rel="noopener noreferrer">
-                            <img src="./src/assets/github.png" alt="GitHub" />
+                            <img src="./public/github.png" alt="GitHub" />
                         </a>
                     </li>
                     <li className="resy">
                         <a href="https://www.resy.com" target="_blank" rel="noopener noreferrer">
-                            <img src="./src/assets/file-alt.svg" alt="Resy" />
+                            <img src="./public/file-alt.svg" alt="Resy" />
                         </a>
                     </li>
                 </ul>
