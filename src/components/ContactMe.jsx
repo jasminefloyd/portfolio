fe78431f1f396c4a2db51e5e4a8db56dfa8ceb53
jasminefloyd <42697356+jasminefloyd@users.noreply.github.com>
2024-11-13 16:49:56 -0500

    v2

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,17 +1,13 @@
 export default function ContactMe() {
     return (
         <div className="contact-section">
-            <div className="contact-left">
-                <h1>Get in touch with me</h1>
-            </div> 
-            <div className="contact-right">
-                <form>
-                    <fieldset>Name</fieldset>
-                    <fieldset>Email Address</fieldset>
-                    <fieldset className="form-message">Message</fieldset>
-                    <button>Contact Me</button>
-                </form>
-            </div> 
+            <h1>Get in touch with me</h1>
+            <form>
+                <fieldset>Name</fieldset>
+                <fieldset>Email Address</fieldset>
+                <fieldset className="form-message">Message</fieldset>
+                <button>Contact Me</button>
+            </form>
         </div>
     )
 }
